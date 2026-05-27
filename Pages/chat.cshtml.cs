a83edc1c36a075099982a36dd10340ce496c77a0
Dingo <foolishdingotv@gmail.com>
2026-05-27 13:45:00 -0600

    Add files via upload

---
+++ Pages/chat.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ARKERATEST1.Pages
+{
+    public class ChatModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
